77b4dbc8def8f0e4c94044661ef886c9ec22b27b
Stephen Doxsee <sdoxsee@gmail.com>
2021-03-29 15:33:12 -0400

    Do GA differently

--- src/components/home/header/Header.js
+++ src/components/home/header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Container } from 'reactstrap';
+import { ReactComponent as Logo } from '../../../assets/logo.svg'
 import './Header.css'
 
 const Header = (props) => {
   return (
     <div className="header border-bottom">
       <Container> 
-          <h2>Environment Variable Generator</h2>
+      {Logo} <h2>Environment Variable Generator</h2>
           <p>for Spring Boot apps</p>
       </Container>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,20 +4,13 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import GA4React from "ga-4-react";
 
-const ga4react = new GA4React("G-Z40RZHFVBY");
-
-(async () => {
-  await ga4react.initialize();
-
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-})();
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
