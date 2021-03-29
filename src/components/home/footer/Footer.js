758ef3bb60fd15902c378b6a346036a22cb2dcb6
Stephen Doxsee <sdoxsee@gmail.com>
2021-03-29 14:11:08 -0400

    More footer

--- src/components/home/footer/Footer.js
+++ src/components/home/footer/Footer.js
@@ -3,23 +3,41 @@ import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import { ReactComponent as Logo } from '../../../assets/logo.svg'
 import intellij from '../../../assets/intellij.png'
+import stephen from '../../../assets/stephen-larger.jpeg'
 import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons' 
 
 
 const Footer = (props) => {
+
+  const twitter = <FontAwesomeIcon icon={faTwitter} />
+  const linkedIn = <FontAwesomeIcon icon={faLinkedin} />
+  const github = <FontAwesomeIcon icon={faGithub} />
+
   return (
   <div className="footer">
     <Container>
-      <Row md="2">
+      <Row lg="2">
         <Col>
-          <p>Find an issue? Have a suggestion? <a alt="Github Repository" href="https://github.com/sdoxsee/env-gen">Pull Requests</a> are welcome!</p>
+        <p>Find an issue? Have a suggestion? </p>
+        <p>{github} <a alt="Github Repository" href="https://github.com/sdoxsee/env-gen">Pull Requests</a> are welcome.</p>
+          <p><img width="400px" alt="Created by Stephen Doxsee" src={stephen}/></p>
           <p>Created by Stephen Doxsee</p>
-          <p>Twitter -> @doxsees, Blogging at https://sdoxsee.github.com</p>
-          
+          <p>
+            <div className="footer-icons">
+            <a href="https://twitter.com/doxsees">{twitter}</a>
+            <a href="https://www.linkedin.com/in/sdoxsee">{linkedIn}</a>
+            <a href="https://sdoxsee.github.com"><Logo style={{height: '20px', width: '20px'}}/></a>
+            <a href="https://github.com/sdoxsee">{github}</a>
+          </div>
+          </p>
+          <h3>Disclaimer</h3>
+          <p>No guarantees about the accuracy of the results. Google Analytics may track traffic but no other data is stored. Use at your own risk.</p>
         </Col>
         <Col>
         <h3>About</h3>
-        <p>Since I enjoy using <a href="https://docs.spring.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features-external-config-relaxed-binding-from-environment-variables">environment variable support</a> to override/set Spring Boot config, I got tired of creating them by hand.</p> 
+        <p>I enjoy using <a href="https://docs.spring.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features-external-config-relaxed-binding-from-environment-variables">environment variable support</a> to override and set Spring Boot configuration. I got tired of creating them by hand so I made this.</p> 
         <p>Why use environment variables?</p>
         <ul>
           <li>
@@ -31,14 +49,14 @@ const Footer = (props) => {
           <li>
             leverage their <i>higher priority</i> over application <a alt="Spring Boot Reference Documentation" href="https://docs.spring.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features-external-config">config data</a>
           </li>
-          <li>easy of use
+          <li>ease of use
             <ul>
               <li>
-                <p>paste into IntelliJ run configuration</p>
+                <p>IntelliJ and other IDEs. (e.g. paste into IntelliJ run configuration)</p>
                 <img width="400px" alt="IntelliJ Run Configuration using Environment Variables" src={intellij}/>
               </li>
               <li>
-                <p>or terminal</p>
+                <p>Terminal</p>
                 <p><div className="code">FOO=bar mvn spring-boot:run</div></p>
               </li>
               <li>
@@ -49,8 +67,11 @@ const Footer = (props) => {
         </ul>
         </Col>
       </Row>
-      <p className="copyright"><Logo style={{height: '50px', width: '50px'}}/>&nbsp;<a alt="Simple Step Solutions" href="https://simplestep.ca">Simple Step Solutions</a> © {new Date().getFullYear()}</p>
+        
     </Container>
+    <br/>
+    <p className="copyright"><Logo style={{height: '50px', width: '50px'}}/>&nbsp;<a alt="Simple Step Solutions" href="https://simplestep.ca">Simple Step Solutions</a> © {new Date().getFullYear()}</p>
+    <br/>
   </div>
   )
 }
