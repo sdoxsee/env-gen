b8c40a509c7a95929dc659ea3de79b184ed0029f
Stephen Doxsee <sdoxsee@gmail.com>
2021-03-30 23:17:17 -0400

    Get ready for different input types

--- src/components/home/main/Main.js
+++ src/components/home/main/Main.js
@@ -7,23 +7,34 @@ import './Main.css'
 
 const Main = (props) => {
 
-  const [yamlText, setYamlText] = useState('foo-bar:\n  baz:\n    - value1\n    - value2\n  enabled: true\nabcDef: value3');
+  const [inputText, setInputText] = useState('foo-bar:\n  baz:\n    - value1\n    - value2\n  enabled: true\nabcDef: value3');
+  // const [inputType, setInputType] = useState('YAML');
+  const [inputType] = useState('YAML');
   const [outputType, setOutputType] = useState('Simple');
   const [properties, setProperties] = useState();
   const [alertText, setAlertText] = useState('');
 
   const onDismiss = () => setAlertText('');
 
-  const applyYamlText = useCallback((yamlText) => {
+  const applyInputText = useCallback((type, text) => {
     try {
-      setYamlText(yamlText);
-      // https://github.com/jusufazer/yaml2properties/blob/master/src/scripts/processor.js
-      const data = yaml.load(yamlText);
-      if (typeof data !== 'object') {
-        throw new Error("I could be wrong, but YAML doesn't seem valid.")
-      }
+      setInputText(text);
+      var deflated
+      // if (type === 'YAML') {
+        // https://github.com/jusufazer/yaml2properties/blob/master/src/scripts/processor.js
+        const data = yaml.load(text);
+        if (typeof data !== 'object') {
+          throw new Error("I could be wrong, but YAML doesn't seem valid.")
+        }
+        deflated = deflate(data) // Convert the JSON structure into an array of strings
+      // } else {
+      //   deflated = text
+      //     .split('\n')
+      //     .map(s => s.trim())
+      //     .filter(String);
+      // }
+      setProperties(deflated); 
       setAlertText('');
-      setProperties(deflate(data)); // Convert the JSON structure into an array of strings
     } catch (e) {
       setAlertText(e.message);
       setProperties([]);
@@ -32,23 +43,27 @@ const Main = (props) => {
   }, []);
 
   useEffect(() => {
-    applyYamlText(yamlText);
-  }, [yamlText, applyYamlText]);
+    applyInputText(inputType, inputText);
+  }, [inputType, inputText, applyInputText]);
 
   const onChangeHandler = event => {
-    applyYamlText(`${event.target.value}`);
+    applyInputText(`${event.target.value}`);
   };
+
+  // const onChangeInputTypeHandler = event => {
+  //   setInputType(`${event.target.value}`);
+  // };
 
   const onChangeOutputTypeHandler = event => {
     setOutputType(`${event.target.value}`);
   };
  
-  var result
+  var outputText
   try {
-    result = properties ? outputFormatter(outputType, properties) : ''
+    outputText = properties ? outputFormatter(outputType, properties) : ''
   } catch (e) {
     console.log(e)
-    result = ''
+    outputText = ''
   }
   
   return (
@@ -57,13 +72,28 @@ const Main = (props) => {
       <Row>
         <Col>
           <div className="code-area">
-              <Label for="exampleText">YAML</Label>
-              <Input type="textarea" name="text" id="exampleText" 
-              value={yamlText}
-              onChange={onChangeHandler}/>
-              <Alert color="danger" isOpen={alertText} toggle={onDismiss}>
-                {alertText}
-              </Alert>
+            <Label for="inputSelect">YAML</Label>
+            <Input type="textarea" name="select1" id="inputSelectMain" 
+            value={inputText}
+            onChange={onChangeHandler}/>
+            <Alert color="danger" isOpen={alertText} toggle={onDismiss}>
+              {alertText}
+            </Alert>
+        {/* <FormGroup row>
+              <Label for="inputSelect" sm={2}>Input</Label>
+              <Col sm={10}>
+                <Input type="select" name="select1" id="inputSelect" onChange={onChangeInputTypeHandler}>
+                  <option>YAML</option>
+                  <option>Properties</option>
+                </Input>
+              </Col>
+            </FormGroup>
+            <Input type="textarea" name="text1" id="inputSelectMain" 
+            value={inputText}
+            onChange={onChangeHandler}/>
+            <Alert color="danger" isOpen={alertText} toggle={onDismiss}>
+              {alertText}
+            </Alert> */}
           </div>
         </Col>
       </Row>
@@ -71,9 +101,9 @@ const Main = (props) => {
         <Col>
           <div className="code-area">
             <FormGroup row>
-              <Label for="exampleSelect" sm={2}>Output</Label>
+              <Label for="outputSelect" sm={2}>Output</Label>
               <Col sm={10}>
-                <Input type="select" name="select" id="exampleSelect" onChange={onChangeOutputTypeHandler}>
+                <Input type="select" name="select2" id="outputSelect" onChange={onChangeOutputTypeHandler}>
                   <option>Simple</option>
                   <option>Shell</option>
                   <option>Kubernetes</option>
@@ -81,8 +111,8 @@ const Main = (props) => {
                 </Input>
               </Col>
             </FormGroup>
-            <Input type="textarea" name="text" id="exampleText" 
-              value={result}
+            <Input type="textarea" name="text2" id="outputSelectMain" 
+              value={outputText}
               onChange={onChangeHandler}/>  
             <br/>
           </div>
